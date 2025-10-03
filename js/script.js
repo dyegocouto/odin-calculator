eb2859cad0d3ce9ce96b3c34e086929cb4b4c501
dyegocouto <175648963+dyegocouto@users.noreply.github.com>
2025-10-03 15:07:28 -0300

    Add listener to all keypad buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 import { add, subtract, multiply, divide } from "./math.js";
+
+const keypad = document.querySelector(".keypad");
+
+function handleClick(event) {
+  const btn = event.target.closest("button");
+  if (!btn) return;
+
+  const value = btn.dataset.value;
+  const type = btn.dataset.type;
+}
+
+keypad.addEventListener("click", handleClick);
