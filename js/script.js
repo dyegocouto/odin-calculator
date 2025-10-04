8e371ba208a3521654a5e9f202d3baab77c1be20
dyegocouto <175648963+dyegocouto@users.noreply.github.com>
2025-10-04 09:28:45 -0300

    Implement number display functionality
    Only the first operand is supported for now.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 import { add, subtract, multiply, divide } from "./math.js";
 
+let currentInput = "";
+
+const currentLine = document.querySelector(".current-line");
 const keypad = document.querySelector(".keypad");
+
+function updateDisplay() {
+  currentLine.textContent = currentInput;
+}
+
+function compute(value, type) {
+  switch (type) {
+    case "number":
+      currentInput += value;
+      updateDisplay();
+      break;
+  }
+}
 
 function handleClick(event) {
   const btn = event.target.closest("button");
@@ -8,6 +24,8 @@ function handleClick(event) {
 
   const value = btn.dataset.value;
   const type = btn.dataset.type;
+
+  compute(value, type);
 }
 
 keypad.addEventListener("click", handleClick);
